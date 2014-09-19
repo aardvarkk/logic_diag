0d0b6ba1fd46d4b42954e718e77a4e91ad1c7cda
aardvarkk <clarksonian@gmail.com>
2014-09-19 17:36:16 -0400

    Demonstrating usage of calculation. Try to match this behaviour.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
+gem 'citrus'
 gem 'treetop'
--- logic_diag.rb
+++ logic_diag.rb
@@ -1,9 +1,17 @@
+require 'citrus'
 require 'treetop'
 
 # NOTE: Text must end in a newline character!
-text = File.read('test.txt')
+# text = File.read('test.txt')
+# Citrus.load 'grammar'
+# parsed = Grammar.parse text
 
-Treetop.load 'grammar'
-
-parsed = GrammarParser.new.parse text
+# Citrus 
+Citrus.load 'calc'
+parsed = Calc.parse '0 + +5 * (3 + (8*7)**9)'
 p parsed.dump
+
+# Treetop
+# Treetop.load 'grammar'
+# parsed = GrammarParser.new.parse text
+# p parsed
