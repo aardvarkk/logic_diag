393b2923780f6843e2f2b39b7c8eaf5e9d75b5e1
aardvarkk <clarksonian@gmail.com>
2014-09-22 14:57:46 -0400

    Working decently well with Parslet.

--- logic_diag.rb
+++ logic_diag.rb
@@ -1,19 +1,62 @@
+require 'awesome_print'
 require 'citrus'
+require 'parslet'
 require 'treetop'
 
-# NOTE: Text must end in a newline character!
 text = File.read('test.txt')
-Citrus.load 'grammar'
-parsed = Grammar.parse text
+
+# # NOTE: Text must end in a newline character!
+# Citrus.load 'grammar'
+# parsed = Grammar.parse text
 
 # Citrus 
 # Citrus.load 'calc'
 # parsed = Calc.parse '(-0  + +5) * (3 + (8*7)**9) / 50 + 3'
 # parsed = Calc.parse '5*4*3'
 
-p parsed.dump
+# p parsed.dump
 
 # Treetop
 # Treetop.load 'grammar'
 # parsed = GrammarParser.new.parse text
 # p parsed
+
+# Parslet
+class LogicGrammar < Parslet::Parser
+  rule(:ws) {
+    match('\\s').repeat(1) 
+  }
+
+  rule(:newline) {
+    match('\\n') 
+  }
+  
+  rule(:identifier) {
+    match('[a-zA-Z0-9]').repeat(1)
+  }
+  
+  rule(:comment_to_eol) { 
+    match('#') >> match('[^#\\n]').repeat 
+  }
+
+  rule(:command_detail) {
+    match('[^#\\n]').repeat(1) 
+  }
+
+  rule(:command) { 
+    identifier.as(:name) >> ws.repeat(1) >> command_detail.as(:detail).repeat >> comment_to_eol.as(:comment).repeat >> newline
+  }
+
+  rule(:commandset) { 
+    command.as(:cmd).repeat(1) 
+  }
+
+  root(:commandset)
+end
+
+begin
+  parsed = LogicGrammar.new.parse text
+rescue Parslet::ParseFailed => error
+  puts error.cause.ascii_tree
+end
+ap parsed
