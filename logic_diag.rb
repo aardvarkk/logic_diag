ad4381f12a30e1057e057516a0ba90fe6ce07256
aardvarkk <clarksonian@gmail.com>
2014-09-25 17:59:10 -0400

    Nice tree output using all strings.

--- logic_diag.rb
+++ logic_diag.rb
@@ -302,7 +302,7 @@ def append_to_tree(hash, idx)
 
   return node
 end
-tree = append_to_tree(forest[-1], 0)
+tree = append_to_tree(forest[2], 0)
 
 # To get this method to work, change RubyTree like this:
 # def print_tree(level = 0, to_print = [:name])
