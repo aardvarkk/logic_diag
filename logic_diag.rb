c8a77662143f714a35f67f004c5a0e2ca92ded19
aardvarkk <clarksonian@gmail.com>
2014-09-26 12:39:42 -0400

    Got tree reordering working.

--- logic_diag.rb
+++ logic_diag.rb
@@ -4,25 +4,6 @@ require 'tree'
 
 text = File.read('test.txt')
 
-def create_vartable(parsed)
-  vartable = []
-  def get_vars(obj)
-    return [] if obj.nil? || !obj.is_a?(Hash)
-    vartable = []
-    # Grab ourselves if we have a var at our level
-    vartable << obj[:var].to_s if obj.key?(:var)
-    # Go into our children if we have any
-    obj.each { |k,v| vartable += get_vars v }
-    vartable
-  end
-  parsed.each do |h|
-    vartable += get_vars h
-  end
-  vartable.uniq!
-  vartable.sort! unless vartable.nil?
-end
-
-# Parslet
 class LogicGrammar < Parslet::Parser
   rule(:ws) {
     # DO NOT use \s for a space -- it seemingly includes newlines!
@@ -156,121 +137,6 @@ begin
 rescue Parslet::ParseFailed => error
   puts error.cause.ascii_tree
 end
-# pp forest
-
-# Create a variable table
-vartable = create_vartable forest
-# pp vartable
-
-# Create a variable table per-tree (to potentially find overlap)
-# forest.each { |t| pp create_vartable [t] }
-
-# To be a leaf node, we must have only one key-pair value of :var => "varname"
-def find_leaf_var(tree, var)
-  return nil if !tree.is_a?(Hash)
-
-  # Try our children first...
-  tree.each do |k,v|
-    leaf_instance = find_leaf_var(v, var)
-    return leaf_instance if leaf_instance
-  end
-
-  # Now examine ourselves...
-  if tree.key?(:var) && tree[:var] == var && tree.keys.count == 1
-    return tree
-  else
-    return nil
-  end
-end
-
-# To be a parent node, we must have one key-pair value of :var => "varname" as well as additional keys
-def find_parent_var(tree, var)
-  return nil if !tree.is_a?(Hash)
-
-  # Try our children first...
-  tree.each do |k,v|
-    parent_instance = find_parent_var(v, var)
-    return parent_instance if parent_instance
-  end
-
-  # Now examine ourselves...
-  if tree.key?(:var) && tree[:var] == var && tree[:val].is_a?(Hash)
-    return tree
-  else
-    return nil
-  end
-end
-
-# To be settled, the forest must NOT contain a matching pair such that one is a leaf node and the other has children
-def settled(forest, vartable)
-  # Go through each variable in the vartable and see if we're able to find it both in a leaf and in a place that has children
-  vartable.each do |v|
-    # pp v
-
-    # Go through every tree in the forest to see if we find a matching pair
-    leaf_instance = nil
-    parent_instance = nil    
-    forest.each do |t|
-      leaf_instance   ||= find_leaf_var(t, v)
-      parent_instance ||= find_parent_var(t, v)
-      return {leaf: leaf_instance, parent: parent_instance} if leaf_instance && parent_instance
-    end
-  end
-
-  return nil
-end
-
-# def remove_subtree(tree, to_remove)
-#   return if !tree.is_a?(Hash)
-  
-#   # Remove from our children first
-#   tree.each { |k,v| remove_subtree(v, to_remove) }
-
-#   # If our value matches the thing to remove, do it
-#   if tree.has_key?(:val) && tree[:val] == to_remove
-#     tree[:val] = to_remove[:var]
-#   end
-# end
-
-def replace_leaf_with_subtree(tree, leaf, subtree)
-  return if !tree.is_a?(Hash)
-
-  # Work on our children
-  tree.each { |k,v| replace_leaf_with_subtree(v, leaf, subtree) }
-
-  # Work on ourselves
-  if tree == leaf
-    tree.replace subtree
-  end
-end
-
-while lp = settled(forest, vartable)
-  
-  # puts "--- FOREST BEFORE"
-  # pp forest
-
-  # puts "--- LEAF"
-  # pp lp[:leaf]
-  # puts "--- PARENT"
-  # pp lp[:parent]
-
-  # First, get rid of the parent entry from the original trees (delete it)
-  # This works because we have a copy
-  # forest.each do |t|
-  #   remove_subtree(t, lp[:parent])
-  # end
-
-  # Then, replace all leaf entries of this variable with parent entries of this variable
-  forest.each do |t|
-    replace_leaf_with_subtree(t, lp[:leaf], lp[:parent])
-  end
-
-  # puts "--- FOREST AFTER"
-  # pp forest
-
-end
-
-# pp forest.last
 
 # Create the RubyTree from a parsed hash
 def append_to_tree(hash, idx)
@@ -323,16 +189,26 @@ tree = append_to_tree(forest[2], 0)
 
 #   children { |child| child.print_tree(level + 1, to_print) if child } # Child might be 'nil'
 # end
-tree.print_tree(0, [:content])
 
-# pp forest.last
-
-def get_deepest(t, depth)
-  return depth-1 if !t.is_a?(Hash)
-  deepest_child = depth
-  t.each { |k,v| deepest_child = [deepest_child, get_deepest(v, depth+1)].max }
-  return deepest_child
+# Reorder the tree such that the deepest side of each node is the left side
+# This is only meaningful for binary operators (OR, AND) that split a node to two children
+def reorder_tree(tree)
+  reordered = tree
+  reordered.each do |n|
+    if n.children.count == 2 && n.children[0].node_height < n.children[1].node_height
+      r = n.children[0]
+      l = n.children[1]
+      n.remove_all!
+      n << l
+      n << r
+    end
+  end
+  return reordered
 end
+
+tree = reorder_tree(tree)
+
+tree.print_tree(0, [:content])
 
 def draw_node(n, file)
   return if !n.is_a?(Hash)
